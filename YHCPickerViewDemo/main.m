ed11eb50bcb73b9599d7f31f16ee2bd294ff6905
Yashesh <user@Yasheshs-Mac-Pro.local>
2012-10-20 17:36:54 +0530

    Initial Commit

---
+++ YHCPickerViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YHCPickerViewDemo
+//
+//  Created by WebAtEase on 20/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
